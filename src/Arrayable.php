6c2eaebd1692595d1c8b24a14f6b98d544893dbe
wells5609 <wellspeterson@live.com>
2014-09-16 17:37:32 -0400

    Create Arrayable.php

---
+++ src/Arrayable.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package wells5609/php-arrayable
+ * @author wells5609
+ * @license MIT
+ * @version 0.1-beta
+ */
+
+/**
+ * Contract for a class that can return its data as an array.
+ * 
+ * @since 0.1-beta
+ */
+interface Arrayable
+{
+
+	/**
+	 * Returns a representation of the object as a native PHP array.
+	 *
+	 * @since 0.1-beta
+	 *
+	 * @return array Associative array of object data.
+	 */
+	public function toArray();
+
+}
